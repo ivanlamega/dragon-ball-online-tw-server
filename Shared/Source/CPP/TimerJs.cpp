fb840eb3637919a488a25d2fa217c5cbbf41bfe3
Ivan Acevedo <ivan.acevedo@sdc.com.ar>
2020-02-21 11:06:25 -0300

    New timer

--- Shared/Source/CPP/TimerJs.cpp
+++ Shared/Source/CPP/TimerJs.cpp
@@ -1,5 +1,47 @@
 #include "TimerJs.h"
 
-void TimerJs::stop() {
+void TimerJs::stop() 
+{
 	this->clear = true;
+}
+
+void TimerJs::stop(int id) 
+{
+	DeleteTimer(id);
+}
+
+void TimerJs::AddTimer(int id) 
+{
+	timers.push_back(id);
+};
+
+void TimerJs::DeleteTimer(int id) {
+	int timer = -1;
+	for (TimersListIt iter = timers.cbegin(); iter != timers.cend(); ++iter)
+	{
+		timer = (*iter);
+
+		if (timer == -1)
+		{
+			continue;
+		}
+
+		if (timer == id)
+		{
+			timers.erase(iter);
+			break;
+		}
+	}
+};
+
+bool TimerJs::FindTimer(int id)
+{
+	for (std::vector<int>::size_type i = 0; i != timers.size(); i++)
+	{
+		if (timers[i] == id)
+		{
+			return true;
+		}
+	}
+	return false;
 }
--- Shared/Source/Header/TimerJs.h
+++ Shared/Source/Header/TimerJs.h
@@ -2,18 +2,38 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 class TimerJs {
 	bool clear = false;
 
+	typedef std::vector<int> TimersList;
+	typedef TimersList::const_iterator TimersListIt;
+	std::vector<int> timers;
+
 public:
+	void AddTimer(int id);
+
+	void DeleteTimer(int id);
+
+	bool FindTimer(int id);
+
 	template<typename A>
 	void setTimeout(A function, int delay);
 	template<typename A>
 	void setInterval(A function, int interval);
+
+	template<typename A>
+	void setTimeout(A function, int delay, int id);
+	template<typename A>
+	void setInterval(A function, int interval, int id);
+
 	template<typename A, typename B>
-	void setIntervalArg(A function, int interval, B argument);
-;	void stop();
+	void setTimeout(A function, int delay, B argument);
+	template<typename A, typename B>
+	void setInterval(A function, int interval, B argument);
+	void stop();
+	void stop(int id);
 
 };
 
@@ -25,6 +45,32 @@ void TimerJs::setTimeout(A function, int delay) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 		if (this->clear) return;
 		function();
+		});
+	t.detach();
+}
+
+template<typename A>
+void TimerJs::setTimeout(A function, int delay, int id) {
+	this->clear = false;
+	AddTimer(id);
+	std::thread t([=]() {
+		if (!FindTimer(id)) return;
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+		if (!FindTimer(id)) return;
+		function();
+		DeleteTimer(id);
+		});
+	t.detach();
+}
+
+template<typename A, typename B>
+void TimerJs::setTimeout(A function, int delay, B argument) {
+	this->clear = false;
+	std::thread t([=]() {
+		if (this->clear) return;
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+		if (this->clear) return;
+		function(argument);
 		});
 	t.detach();
 }
@@ -43,8 +89,23 @@ void TimerJs::setInterval(A function, int interval) {
 	t.detach();
 }
 
+template<typename A>
+void TimerJs::setInterval(A function, int interval, int id) {
+	this->clear = false;
+	AddTimer(id);
+	std::thread t([=]() {
+		while (true) {
+			if (!FindTimer(id)) return;
+			std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+			if (!FindTimer(id)) return;
+			function();
+		}
+		});
+	t.detach();
+}
+
 template<typename A, typename B>
-void TimerJs::setIntervalArg(A function, int interval, B argument) {
+void TimerJs::setInterval(A function, int interval, B argument) {
 	this->clear = false;
 	std::thread t([=]() {
 		while (true) {
